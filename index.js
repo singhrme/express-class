7b8faa65f10f831dd669c8f81e35283936f57851
singhrme <singhr7941@gmail.com>
2025-10-04 19:42:53 +0530

    adding envirement variable

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
-const port = 3000;
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+const port = process.env.PORT || 8000;
 const app = express();
 
 // middleware
